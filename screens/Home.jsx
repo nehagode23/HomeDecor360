c123d60ad536b39156c710648e18e752856b2190
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2024-04-02 23:35:32 +0530

    bottom tabnavigation done

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,15 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Search = () => {
+const Home = () => {
     return(
         <View>
-            <Text>Search</Text>
+            <Text>Home</Text>
         </View>
     )
 }
 
-export default Search
+export default Home
 
 const styles= StyleSheet.create({
 

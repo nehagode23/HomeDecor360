07f89e338db3ede42649a0ab0e2cc3051d59734b
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2024-04-11 22:35:51 +0530

    heading

--- components/home/heading.style.js
+++ components/home/heading.style.js
@@ -3,13 +3,20 @@ import { COLORS, SIZES } from "../../constants";
 
 const styles = StyleSheet.create({
     container:{
+        marginTop:SIZES.medium,
+        //marginBottom:-SIZES.xSmall,
+        marginHorizontal:12
 
     },
     header:{
+        flexDirection:"row",
+        justifyContent:"space-between",
 
     },
     headerTitle:{
-        
+        fontFamily:"semibold",
+        fontSize:SIZES.xLarge -2,
+
     }
 
 })

3cddd809f5212a6770b0fd58ade0e6df5dbbe23d
nehagode23 <118003687+nehagode23@users.noreply.github.com>
2024-04-21 08:27:47 +0530

    routes setup

--- backend/models/Products.js
+++ backend/models/Products.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 const ProductSchema= new mongoose.Schema({
     title:{type: String, require:true},
     supplier:{type: String, require:true},
+    price:{type: String, require:true},
     imageUrl:{type: String, require:true},
     description:{type: String, require:true},
     product_location:{type: String, require:true},
